78b54da45e4c7442ce0edd08780b29bcd89622e1
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2021-07-28 19:20:32 -0500

    deploying to heroku on my own

---
+++ temp.js
@@ -0,0 +1,91 @@
+const form = document.querySelector('.top-banner form');
+const input = document.querySelector('.top-banner input');
+const msg = document.querySelector('.top-banner .msg');
+const list = document.querySelector('.building .cities');
+const apiKey = '1dca8b306a1b627e8d7c4dfd8813b78e';
+let index = 0;
+
+$(document).ready(function () {
+  $('button').click(function () {
+    $('p').hide();
+  });
+});
+
+$(document).ready(function () {
+  /*! Fades in page on load */
+  $('body').css('display', 'none');
+  $('body').fadeIn(5000);
+});
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log();
+  let inputVal = input.value;
+
+  const listItems = list.querySelectorAll('.building .city');
+  const listItemsArray = Array.from(listItems);
+
+  if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter((el) => {
+      let content = '';
+
+      if (inputVal.includes(',')) {
+        if (inputVal.split(',')[1].length > 2) {
+          inputVal = inputVal.split(',')[0];
+          content = el
+            .querySelector('.city-name span')
+            .textContent.toLowerCase();
+        } else {
+          content = el.querySelector('.city-name').dataset.name.toLowerCase();
+        }
+      } else {
+        content = el.querySelector('.city-name span').textContent.toLowerCase();
+      }
+      return content == inputVal.toLowerCase();
+    });
+
+    if (filteredArray.length > 0) {
+      msg.textContent = `You already know the weather for ${
+        filteredArray[0].querySelector('.city-name span').textContent
+      } ...add country code to be more specific`;
+      form.reset();
+      input.focus();
+      return;
+    }
+  }
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=imperial`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://openweathermap.org/img/wn/${weather[0]['icon']}@2x.png`;
+
+      const li = document.createElement('li');
+      li.classList.add('city');
+
+      const markup = `
+  <h2 class="city-name" data-name="${name},${sys.country}">
+    <span>${name}</span>
+    <sup>${sys.country}</sup>
+  </h2>
+  <div class="city-temp">${Math.round(main.temp)}<sup>°F</sup>
+  </div>
+  <figure>
+    <img class="city-icon" src=${icon} alt=${weather[0]['main']}>
+    <figcaption>${weather[0]['description']}</figcaption>
+  </figure>
+`;
+      $('.building .cities').append(
+        `<li id='city${index}' class='city'>${markup}</li>`
+      );
+      $(`#city${index}`).hide().fadeIn(5000);
+      index++;
+    })
+    .catch(() => {
+      msg.textContent = 'City not valid';
+    });
+  msg.textContent = '';
+  form.reset();
+  input.focus();
+});
